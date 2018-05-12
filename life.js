e9ff8339f8858d42dbd9063a892fe750287911d4
Jarren San Jose <sanjose.jarren@gmail.com>
2018-05-11 22:37:31 -0700

    weeks or days

--- life.js
+++ life.js
@@ -1,15 +1,14 @@
-var life;
 var today_date = document.querySelector("#today_date");
 var birthday_date = document.querySelector("#birthday");
 var judgement_date = document.querySelector("#judgement");
 var submitButton = document.querySelector("#submit");
 var timeUsed = document.querySelector("#timeUsed");
 var timeLeft = document.querySelector("#timeRemaining");
+var weeks = document.querySelector("#weeks");
+var days = document.querySelector("#days");
 
-// square boxes by day, weeks, or months
 var const_day = 1000 * 60 * 60 * 24; // ms in a day
 var const_week = 1000 * 60 * 60 * 24 * 7; // ms in a week
-// toggle to see by day or weeks
 
 var today = new Date();
 var today_month = ((today.getMonth() + 1) < 10) ? "0" + (today.getMonth() + 1) : (today.getMonth() + 1);
@@ -27,22 +26,21 @@ submitButton.addEventListener("click", function () {
   var birthdayUtc = Date.UTC(birthday.getFullYear(), birthday.getMonth(), birthday.getDate());
   var judgementUtc = Date.UTC(judgement.getFullYear(), judgement.getMonth(), judgement.getDate());
 
-  life = Math.floor((judgementUtc - birthdayUtc) / const_day);
-  var life_weeks = Math.floor((judgementUtc - birthdayUtc)) / const_week;
+  let divideBy = weeks.checked ? const_week : const_day;
 
   var todayUtc = Date.UTC(today.getFullYear(), today.getMonth(), today.getDate());
-  var life_completed = Math.floor((todayUtc - birthdayUtc) / const_day);
-  var life_left = Math.floor((judgementUtc - todayUtc) / const_day);
+  var completed = Math.floor((todayUtc - birthdayUtc) / divideBy);
+  var left = Math.floor((judgementUtc - todayUtc) / divideBy);
 
-  var life_completed_output = "";
-  var life_left_output = "";
-  for(let i = 0; i < life_completed; ++i) {
-    life_completed_output += "<div class = \"completed\"></div>";
+  var completed_output = "";
+  var left_output = "";
+  for(let i = 0; i < completed; ++i) {
+    completed_output += "<div class = \"completed\"></div>";
   }
-  for(let i = 0; i < life_left; ++i) {
-    life_left_output += "<div class = \"remaining\"></div>";
+  for(let i = 0; i < left; ++i) {
+    left_output += "<div class = \"remaining\"></div>";
   }
 
-  timeUsed.innerHTML = life_completed_output;
-  timeLeft.innerHTML = life_left_output;
+  timeUsed.innerHTML = completed_output;
+  timeLeft.innerHTML = left_output;
 });
